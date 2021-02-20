2db9099bd473690a46ab0120adee019465d5077d
obol007 <obol007@gmail.com>
2021-02-20 11:53:46 +0100

    Logo added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,6 +3,7 @@ import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from './../Navigation/Toolbar/Toolbar';
 
+
 const layout = (props) => (
     <Aux>
         <Toolbar />
